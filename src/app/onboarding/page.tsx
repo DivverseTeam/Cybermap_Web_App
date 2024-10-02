ee1057ffeba9ede6c15b8e895766a7a76ec6a29e
veyron-davids <abiolaajileye@gmail.com>
2024-10-02 23:00:23 +0100

    fixes

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React from 'react'
+import OnboardingPage from '~/containers/onboarding'
+
+export default function page() {
+  return (
+    <OnboardingPage />
+  )
+}
---
+++ src/containers/onboarding/components/FormGroup.tsx
@@ -0,0 +1,9 @@
+import OrganizationDetails from "./OrganizationDetails";
+
+export default function FormGroup() {
+  return (
+    <div className="w-full">
+      <OrganizationDetails />
+    </div>
+  );
+}
