bfc7cb4387c6e6e11aeb336a4c8284755f200c83
Timothy Mbachu <follow.timothy@gmail.com>
2024-11-01 10:31:11 +0100

    Feat/controls UI (#18)
    * set up shadcn data table for evidences, database integration pending...
    
    * add constant as datasource
    
    * fix table pagination
    
    * added evidence library
    
    * added evidence library trying to push
    
    * added evidence library
    
    * added integrations page
    
    * build error fixes
    
    * image dimensions fixes
    
    * fixes and improvements
    
    * set up controls page
    
    * controls page setup done, pending framework checkbox filtering
    
    * fixes to controls page ui
    
    * fixes to controls page
    
    * fixes
    
    ---------
    
    Co-authored-by: John Afolabi <johnafolabi.da@gmail.com>

--- src/app/(main)/personnel/page.tsx
+++ src/app/(main)/personnel/page.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import PageTitle from "~/containers/dashboard/components/PageTitle";
 
 export default function PersonnelPage() {
-  return (
-    <div className="h-full">
-      <PageTitle title="Personnel" description="" />
-    </div>
-  );
+  return <div className="h-full">Personnel</div>;
 }
