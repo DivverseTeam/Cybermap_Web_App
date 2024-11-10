a13ea1a03549957887dec761be451f1ec875dd87
Timothy Mbachu <follow.timothy@gmail.com>
2024-11-11 00:32:16 +0100

    fix to sidebar nav link's blue vertical line

--- src/app/_components/ui/nav.tsx
+++ src/app/_components/ui/nav.tsx
@@ -11,96 +11,98 @@ import { type ElementType, MouseEvent, type ReactNode } from "react";
 import { cn } from "~/lib/utils";
 
 interface LinkProp {
-	// title: string;
-	// label?: string;
-	// icon?: ElementType; // Icon as a React element
-	// onClick?: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void; // Typing for onClick event handler
-	// variant?: "default" | "ghost" | "lightBlue";
-	// href?: string | undefined;
-	title: string;
-	label?: string;
-	href?: string;
-	icon?: ElementType;
-	variant: "lightBlue" | "ghost" | "default"; // Adjust to the allowed values
-	onClick?: () => void;
+  // title: string;
+  // label?: string;
+  // icon?: ElementType; // Icon as a React element
+  // onClick?: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void; // Typing for onClick event handler
+  // variant?: "default" | "ghost" | "lightBlue";
+  // href?: string | undefined;
+  title: string;
+  label?: string;
+  href?: string;
+  icon?: ElementType;
+  variant: "lightBlue" | "ghost" | "default"; // Adjust to the allowed values
+  onClick?: () => void;
 }
 interface NavProps {
-	isCollapsed: boolean;
-	links: LinkProp[];
-	groupName: string;
+  isCollapsed: boolean;
+  links: LinkProp[];
+  groupName: string;
 }
 
 export function Nav({ links, groupName, isCollapsed }: NavProps) {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	return (
-		<div
-			data-collapsed={isCollapsed}
-			className="group flex flex-col gap-2 py-2 text-[#80828D] text-sm data-[collapsed=true]:py-2 "
-		>
-			{!["home", "actions"].includes(groupName.toLocaleLowerCase()) && (
-				<span className="mt-[-16px] font-[500] text-[#B9BBC6] text-sm uppercase [@media(min-width:1300px)]:mt-[-10px]">
-					{groupName}
-				</span>
-			)}
-			{groupName.toLocaleLowerCase() === "actions" && (
-				<div className="mt-4 2xl:mt-11 [@media(min-width:1300px)]:mt-8"></div>
-			)}
-			<nav className=" grid gap-1 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
-				{links.map((link: LinkProp, index) => {
-					let isActive = false;
+  return (
+    <div
+      data-collapsed={isCollapsed}
+      className="group flex flex-col gap-2 py-2 data-[collapsed=true]:py-2 text-sm text-[#80828D] "
+    >
+      {!["home", "actions"].includes(groupName.toLocaleLowerCase()) && (
+        <span className="uppercase font-[500] text-[#B9BBC6] text-sm mt-[-16px] [@media(min-width:1400px)]:mt-[-10px]">
+          {groupName}
+        </span>
+      )}
+      {groupName.toLocaleLowerCase() === "actions" && (
+        <div className="mt-4 [@media(min-width:1400px)]:mt-8 2xl:mt-11"></div>
+      )}
+      <nav className=" grid gap-1 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+        {links.map((link: LinkProp, index) => {
+          let isActive = false;
 
-					if (link.href === "/dashboard") {
-						isActive = pathname === "/dashboard"; // Only active if exactly on /dashboard
-					} else {
-						// For other routes like /dashboard/frameworks and /settings
-						isActive = pathname.startsWith(link.href!);
-					}
-					return (
-						<div className="relative flex items-center" key={index}>
-							{isActive && (
-								<div className="absolute left-[-16] h-9 w-1 rounded-md bg-primary 2xl:w-1.5 [@media(min-width:1300px)]:left-[-25]"></div>
-							)}
-							<Link
-								key={index}
-								href={`${link.href}`}
-								className={cn(
-									"w-[220px]",
-									isActive && "text-black",
-									buttonVariants({
-										variant: isActive ? "lightBlue" : "ghost",
-										size: "sm",
-									}),
-									link.variant === "default" &&
-										"dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white",
-									link.variant === "lightBlue" && "text-black",
-									"justify-start",
-								)}
-							>
-								{link.icon && (
-									<link.icon
-										className={`${
-											isActive && "text-primary"
-										} "mr-2 h-4 w-4" pr-2`}
-									/>
-								)}
-								{link.title}
-								{link.label && (
-									<span
-										className={cn(
-											"ml-auto",
-											link.variant === "default" &&
-												"text-background dark:text-white",
-										)}
-									>
-										{link.label}
-									</span>
-								)}
-							</Link>
-						</div>
-					);
-				})}
-			</nav>
-		</div>
-	);
+          if (link.href === "/dashboard") {
+            isActive = pathname === "/dashboard"; // Only active if exactly on /dashboard
+          } else {
+            // For other routes like /dashboard/frameworks and /settings
+            isActive = pathname.startsWith(link.href!);
+          }
+          return (
+            <div className="flex items-center relative" key={index}>
+              {isActive && (
+                <div className="absolute right-[258px] [@media(min-width:1400px)]:right-[250px] h-9 w-1 rounded-md bg-primary 2xl:w-1.5"></div>
+              )}
+              <div className="flex items-center">
+                <Link
+                  key={index}
+                  href={`${link.href}`}
+                  className={cn(
+                    "w-[220px]",
+                    isActive && "text-black",
+                    buttonVariants({
+                      variant: isActive ? "lightBlue" : "ghost",
+                      size: "sm",
+                    }),
+                    link.variant === "default" &&
+                      "dark:bg-muted dark:text-white dark:hover:bg-muted  dark:hover:text-white",
+                    link.variant === "lightBlue" && "text-black",
+                    "justify-start"
+                  )}
+                >
+                  {link.icon && (
+                    <link.icon
+                      className={`${
+                        isActive && "text-primary"
+                      } "mr-2 pr-2 h-4 w-4"`}
+                    />
+                  )}
+                  {link.title}
+                  {link.label && (
+                    <span
+                      className={cn(
+                        "ml-auto",
+                        link.variant === "default" &&
+                          "text-background dark:text-white"
+                      )}
+                    >
+                      {link.label}
+                    </span>
+                  )}
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </nav>
+    </div>
+  );
 }
