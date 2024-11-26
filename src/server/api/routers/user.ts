8c75069fef47c536496fb7672c3775d64a0bbb5b
John Afolabi <johnafolabi.da@gmail.com>
2024-11-26 14:29:46 +0100

    add control model, save controls (#40)

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -17,6 +17,9 @@ import User, { User as UserSchema } from "~/server/models/User";
 import { signIn, signUp } from "./actions";
 import { FrameworkName } from "~/lib/types";
 
+import { controls } from "~/lib/constants/controls";
+import Control, { OrganisationControl } from "~/server/models/Control";
+
 export const SignInProps = z.object({
   email: z.string(),
   password: z.string(),
@@ -61,8 +64,33 @@ export const userRouter = createTRPCRouter({
           user: { id: userId },
         },
       } = ctx;
+      const { frameworks = [] } = input;
 
       const organisationId = new mongoose.Types.ObjectId().toString();
+
+      const upsertControlsPromises: Array<Promise<unknown>> = [];
+
+      controls.forEach((control) => {
+        if (
+          control.mapped.some((framework) => frameworks.includes(framework))
+        ) {
+          upsertControlsPromises.push(
+            Control.updateOne(
+              {
+                code: control.code,
+              },
+              {
+                $set: {
+                  ...control,
+                  organisationId,
+                  status: "NOT_IMPLEMENTED",
+                },
+              },
+              { upsert: true },
+            ),
+          );
+        }
+      });
 
       const [_, updatedUser] = await Promise.all([
         Organisation.create({
@@ -70,6 +98,7 @@ export const userRouter = createTRPCRouter({
           ...input,
         }),
         User.findByIdAndUpdate(userId, { organisationId }, { new: true }),
+        ...upsertControlsPromises,
       ]);
 
       return updatedUser?.toObject();
